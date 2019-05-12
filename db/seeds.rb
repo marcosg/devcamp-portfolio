2301317df7cd9156aef737b6c1df467c9a90e7ea
marcosg <marcosg101@gmail.com>
2019-05-12 08:43:11 -0400

    Integrated Techology model

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,3 +49,11 @@ end
   )
 end
 puts "9 portfolio items created"
+
+3.times do |tech|
+  Portfolio.last.technologies.create(
+                                 name: "Technology #{tech}"
+  )
+  puts '3 technologies created'
+
+end
